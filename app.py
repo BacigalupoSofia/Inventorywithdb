65352cd128f1df048c4381505fd20779863b15f7
BacigalupoSofia <bacigalupo.ie@gmail.com>
2026-09-03 22:14:23 +0100

    Create product model and database table

--- app.py
+++ app.py
@@ -8,9 +8,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    manufacturer = db.Column(db.String(50), nullable=False)
+    stock_quantity = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
+
 @app.route('/')
 def home():
     return 'HOLA DATABASE'
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
