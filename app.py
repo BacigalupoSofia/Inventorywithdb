6eb39fdc8b90c6c85abf605449dd3ec387f7c78c
BacigalupoSofia <bacigalupo.ie@gmail.com>
2026-09-09 20:57:39 +0100

    Add form moved to an independent html file

--- app.py
+++ app.py
@@ -18,34 +18,45 @@ class Product(db.Model):
     def __repr__(self):
         return f'<Product {self.name}>'
 
+#Main page of the app
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# display of Stock in the dental practice 
+
 @app.route('/stock', methods=['GET', 'POST'])
 def stock():
-
-    if request.method == 'POST':
-
-        name = request.form['name']
-        description = request.form['description']
-        manufacturer = request.form['manufacturer']
-        stock_quantity = int(request.form['quantity'])
-
-        new_product = Product(
-            name=name,
-            description=description,
-            manufacturer=manufacturer,
-            stock_quantity=stock_quantity,
-        )
-        db.session.add(new_product)
-        db.session.commit()
-
-        return redirect(url_for('stock'))
 
     products = Product.query.all() 
     return render_template('index.html', products=products)
 
+# Add new product to the stock
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_product():
+
+    if request.method == 'POST':
+    
+            name = request.form['name']
+            description = request.form['description']
+            manufacturer = request.form['manufacturer']
+            stock_quantity = int(request.form['quantity'])
+    
+            new_product = Product(
+                name=name,
+                description=description,
+                manufacturer=manufacturer,
+                stock_quantity=stock_quantity,
+            )
+            db.session.add(new_product)
+            db.session.commit()
+    
+            return redirect(url_for('stock'))
+    return render_template('add.html')
+
+# Delete from database or add or remove 1 item from the stock quantity 
 
 @app.route('/delete/<int:id>')
 def delete_product(id):
@@ -53,6 +64,30 @@ def delete_product(id):
     db.session.delete(product)
     db.session.commit()
     return redirect(url_for('stock'))
+
+@app.route('/minus/<int:id>')
+def minus_stock(id):
+    product = Product.query.get_or_404(id)
+    product.stock_quantity -= 1
+    db.session.commit()
+    return redirect(url_for('stock'))
+
+@app.route('/plus/<int:id>')
+def plus_stock(id):
+
+    product = Product.query.get_or_404(id)
+    product.stock_quantity += 1
+    db.session.commit()
+    return redirect(url_for('stock'))
+
+
+# View product details
+
+@app.route('/details/<int:id>')
+def details(id):
+    product = Product.query.get_or_404(id)
+    return render_template('details.html', product=product)
+
 
 if __name__ == '__main__':
     with app.app_context():
